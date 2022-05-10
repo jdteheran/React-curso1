9b9b4fa75cac0680d42f8237c196bf5e89d5b342
Juan Diego Teheran Olmos <jdteheran@uninorte.edu.co>
2022-05-10 18:29:51 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import Fruta from './components/Fruta'
+import React from 'react';
+import Fruta from './components/Fruta' 
 
-const App = () => {
-    return (
-        <>
-            <Fruta />
-            <Fruta />
-            <Fruta />
-        </>
-    )
+class App extends React.Component {
+
+    state = {
+        total: 0
+    }
+
+    total = (parametro) => {
+        console.log(parametro);
+       // this.setState({total: (this.state.total - parametro) + this.state.total})
+    }
+
+    render() {
+        return (
+            <>
+                <Fruta name='Manzana' price={4} bus={this.total}/>
+                <Fruta name='Fresa' price={3} bus={this.total}/>
+                <Fruta name='Mango' price={5} bus={this.total}/>
+    
+                <p><strong>Gran Total:</strong> {this.state.total}</p>
+            </>
+        )
+    }
 }
 
 export default App
--- src/components/Fruta/Fruta.js
+++ src/components/Fruta/Fruta.js
@@ -2,17 +2,64 @@ import React from 'react'
 
 class Fruta extends React.Component {
 
+    state = {
+        cantidad: 0
+    }
+
+    agregar = () => {
+
+        const nueva_cantidad = this.state.cantidad + 1
+
+        this.props.bus({
+            id: this.props.name,
+            cantidad: nueva_cantidad,
+            precio: this.props.price
+        })
+
+        this.setState({
+            cantidad: nueva_cantidad
+        })
+    }
+
+    quitar = () => {
+        const nueva_cantidad = this.state.cantidad - 1
+
+        this.props.bus({
+            id: this.props.name,
+            cantidad: nueva_cantidad,
+            precio: this.props.price
+        })
+
+        this.setState({
+            cantidad: nueva_cantidad
+        })
+    }
+
+    limpiar = () => {
+        const nueva_cantidad = 0
+
+        this.props.bus({
+            id: this.props.name,
+            cantidad: nueva_cantidad,
+            precio: this.props.price
+        })
+
+        this.setState({
+            cantidad: nueva_cantidad
+        })
+    }
+
     render() {
         return (
             <div>
-                <h3>Titulo</h3>
-                <p>Precio</p>
-                <p>Cantidad</p>
-                <p><strong>Total:</strong></p>
+                <h3>{this.props.name}</h3>
+                <p>Precio: {this.props.price}</p>
+                <p>Cantidad: {this.state.cantidad} </p>
+                <p><strong>Total:</strong> {this.props.price * this.state.cantidad}</p>
 
-                <button>+</button>
-                <button>-</button>
-                <button>Limpiar</button>
+                <button onClick={this.agregar}>+</button>
+                <button onClick={this.quitar}>-</button>
+                <button onClick={this.limpiar}>Limpiar</button>
                 <hr/>
             </div>
         )

c1d15a88f1341de49fd145d116488c9c8395d615
Juan Diego Teheran Olmos <jdteheran@uninorte.edu.co>
2022-05-16 19:15:59 -0500

    rutas

--- src/App.js
+++ src/App.js
@@ -2,42 +2,23 @@ import React, {useState, useEffect} from 'react';
 import Fruta from './components/Fruta'
 import CheckIn from './components/CheckIn';
 
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
 const App = () => {
     
-    const [state, setState] = useState({
-        total: {
-            manzana: 0,
-            fresa: 0,
-            mango: 0
-        }
-    });
-
-    useEffect(() => {
-        console.log(state)
-    },[state.total.fresa])
-    
-
-    const total = (parametro) => {
-        if (parametro.id == "Manzana") {
-            setState({total: {...state.total,manzana: parametro.precio * parametro.cantidad}})
-        } 
-        if (parametro.id == "Fresa") {
-            setState({total: {...state.total,fresa: parametro.precio * parametro.cantidad}})
-        }
-        if (parametro.id == "Mango") {
-            setState({total: {...state.total,mango: parametro.precio * parametro.cantidad}})
-        }
-    }
-
-    const {manzana, fresa, mango} = state.total
     return (
-        <>
-            <Fruta name='Manzana' price={4} bus={total}  />
-            <Fruta name='Fresa' price={3} bus={total} />
-            <Fruta name='Mango' price={5} bus={total} />
-
-            <CheckIn total={manzana + fresa + mango} />
-        </>
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Fruta name="Manzana" price={4} />}>
+            </Route>
+            <Route path="/hola" element={<CheckIn total={4500} />}>
+            </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
